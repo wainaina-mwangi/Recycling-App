2670b6eb77a9d0419716e236fe82503b700411e0
Wainaina Erastus <wainainaerastus2@gmail.com>
2025-07-17 02:25:56 -0700

    mockData

---
+++ client/my-app/src/mockData/data.js
@@ -0,0 +1,32 @@
+export const NavbarMenu = [
+    {
+        id:1,
+        title: "Home",
+        link : "/"
+    },
+
+    {
+        id:2,
+        title: "Find Recyclers",
+        link : "#"
+    },
+
+
+    {
+        id:3,
+        title: "Request Pickup",
+        link : "#"
+    },
+
+    {
+        id:4,
+        title: "Our sponsors",
+        link : "#"
+    },
+
+    {
+        id:5,
+        title: "FAQS",
+        link : "#"
+    },
+]
