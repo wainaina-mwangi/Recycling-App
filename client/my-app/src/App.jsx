91c3ad3afea9a8a694b8b30f6b7ab651352bd156
Wainaina Erastus <wainainaerastus2@gmail.com>
2025-07-17 02:03:40 -0700

    first commit

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+// import Navbar from './Components/Navbar'
+
+function App() {
+  return (
+    <div className ='overflow-x-hidden'>
+      <Navbar/>
+    </div>
+  )
+}
+
+export default App
